311befa9f6ff99133f29a4f0ebf5e90da9f3ad6a
John Doe <johndoe@email.com>
2024-04-04 12:41:00 -0400

    Starting code to print menu

--- crypto-trading-platform/main.cpp
+++ crypto-trading-platform/main.cpp
@@ -2,8 +2,17 @@
 
 int main()
 {
-	
-	
-	
+	// 1 print help
+	std::cout << "1: Print Help" << std::endl;
+	// 2 print exchange stats
+	std::cout << "2: Print Exchange Stats" << std::endl;
+	// 3 make an offer
+	std::cout << "3: Make an offer" << std::endl;
+	// 4 make a bid
+	std::cout << "4: Make a bid" << std::endl;
+	// 5 print wallet
+	std::cout << "5: Print Wallet" << std::endl;
+	// 6 continue
+	std::cout << "6: Continue" << std::endl;
 	return 0;
 }
