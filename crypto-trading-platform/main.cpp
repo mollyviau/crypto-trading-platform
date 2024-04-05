a59acd6b17194cd1c60c0999c8463294b0849de1
John Doe <johndoe@email.com>
2024-04-05 12:05:31 -0400

    Refactored if statement for menu options into a switch statement.

--- crypto-trading-platform/main.cpp
+++ crypto-trading-platform/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
 
 int main()
 {
 	while (true) 
 	{
 		// 1 print help
-		std::cout << "1: Print Help" << std::endl;
+		cout << "1: Print Help" << std::endl;
 		// 2 print exchange stats
-		std::cout << "2: Print Exchange Stats" << std::endl;
+		cout << "2: Print Exchange Stats" << std::endl;
 		// 3 make an offer
-		std::cout << "3: Make an offer" << std::endl;
+		cout << "3: Make an offer" << std::endl;
 		// 4 make a bid
-		std::cout << "4: Make a bid" << std::endl;
+		cout << "4: Make a bid" << std::endl;
 		// 5 print wallet
-		std::cout << "5: Print Wallet" << std::endl;
+		cout << "5: Print Wallet" << std::endl;
 		// 6 continue
-		std::cout << "6: Continue" << std::endl;
+		cout << "6: Continue" << std::endl;
 		// 7 exit
-		std::cout << "7: Exit" << std::endl;
+		cout << "7: Exit" << std::endl;
 
 
 		std::cout << "=============" << std::endl;
@@ -27,39 +31,36 @@ int main()
 		std::cin >> userOption;
 		std::cout << "You chose: " << userOption << std::endl;
 
-		if (userOption == 0) // bad input
-		{
+		switch (userOption) {
+		case 0: 
 			std::cout << "Invalid choice, choose 1-7." << std::endl;
-		}
-		if (userOption == 1)
-		{
-			std::cout << "Help: Your aim is to make money, analyse the market and make bids and offers." << std::endl;
-		}
-		if (userOption == 2)
-		{
-			std::cout << "Print Exchange Stats. Market looks good." << std::endl;
-		}
-		if (userOption == 3)
-		{
-			std::cout << "Make an offer" << std::endl;
-		}
-		if (userOption == 4)
-		{
-			std::cout << "Make a bid." << std::endl;
-		}
-		if (userOption == 5)
-		{
-			std::cout << "Print Wallet. Here's your wallet: $xx,xxx" << std::endl;
-		}
-		if (userOption == 6)
-		{
-			std::cout << "Going to next time frame." << std::endl;
-		}
-		if (userOption == 7)
-		{
-			std::cout << "Exiting, Goodbye Trader!" << std::endl;
-
 			break;
+		case 1:
+			std::cout << "Help: Your aim is to make money, analyse the market and make bids and offers." << std::endl;
+			break;
+		case 2:
+			std::cout << "Print Exchange Stats. Market looks good." << std::endl;
+			break;
+		case 3:
+			std::cout << "Make an offer." << std::endl;
+			break;
+		case 4:
+			std::cout << "Make a bid." << std::endl;
+			break;
+		case 5:
+			std::cout << "Print Wallet. Here's your wallet: $xx,xxx" << std::endl;
+			break;
+		case 6:
+			std::cout << "Going to next time frame." << std::endl;
+			break;
+		case 7:
+			std::cout << "Exiting, Goodbye Trader!" << std::endl;
+			// Exit the program
+			exit(0);
+			break;
+		default:
+			std::cout << "Invalid choice\n";
+			
 		}
 	}
 
